1125f273c6e780b9416fc100a8fb3ce57a75d80a
hmrm <hmrmaxwell@gmail.com>
2012-07-09 14:59:15 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduler::Application.config.secret_token = '749c57862019dddf37cbd8f3fe803f3d10eb1e8e2693f2a999999c73ba7891ce6326d5dedb5e30a8c0348dbac7cb24d8382a8db6952cee9233113e26fbb5c15f'
