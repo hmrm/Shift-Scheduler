c4462ae5cb2701199c962eb6c449d56caa2dde3c
hmrm <hmrmaxwell@gmail.com>
2012-07-10 14:09:44 -0700

    messing with stuff

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -6,18 +6,6 @@ class TutorsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @tutors }
-    end
-  end
-
-  # GET /tutors/1
-  # GET /tutors/1.json
-  def show
-    @tutor = Tutor.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @tutor }
     end
   end
 
@@ -28,13 +16,7 @@ class TutorsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @tutor }
     end
-  end
-
-  # GET /tutors/1/edit
-  def edit
-    @tutor = Tutor.find(params[:id])
   end
 
   # POST /tutors
@@ -44,27 +26,9 @@ class TutorsController < ApplicationController
 
     respond_to do |format|
       if @tutor.save
-        format.html { redirect_to @tutor, notice: 'Tutor was successfully created.' }
-        format.json { render json: @tutor, status: :created, location: @tutor }
+        format.html { redirect_to tutors_url, notice: 'Tutor was successfully created.' }
       else
         format.html { render action: "new" }
-        format.json { render json: @tutor.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /tutors/1
-  # PUT /tutors/1.json
-  def update
-    @tutor = Tutor.find(params[:id])
-
-    respond_to do |format|
-      if @tutor.update_attributes(params[:tutor])
-        format.html { redirect_to @tutor, notice: 'Tutor was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @tutor.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -77,7 +41,6 @@ class TutorsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to tutors_url }
-      format.json { head :no_content }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Scheduler::Application.routes.draw do
   resources :tutors
 
-  get "home/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +50,7 @@ Scheduler::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'tutors#index'
 
   # See how all your routes lay out with "rake routes"
 
