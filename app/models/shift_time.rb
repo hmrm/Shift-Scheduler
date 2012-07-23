e7d3335af7da8838f2f3c9ad7f71702db7a4ae28
hmrm <hmrmaxwell@gmail.com>
2012-07-23 13:46:11 -0700

    whoops

--- app/models/shift_time.rb
+++ app/models/shift_time.rb
@@ -3,5 +3,5 @@ class ShiftTime < ActiveRecord::Base
 
   has_many :availabilitys, :dependent => :destroy
 
-  valdates :time_of_day, :uniqueness => { :scope => :day_of_week }
+  validates :time_of_day, :uniqueness => { :scope => :day_of_week }
 end
