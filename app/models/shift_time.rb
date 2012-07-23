a98a9365615f9c3055305821beb110229d0241c8
hmrm <hmrmaxwell@gmail.com>
2012-07-23 13:42:34 -0700

    add validators for max hours and uniqueness of shifts

--- app/models/shift_time.rb
+++ app/models/shift_time.rb
@@ -2,4 +2,6 @@ class ShiftTime < ActiveRecord::Base
   attr_accessible :day_of_week, :time_of_day
 
   has_many :availabilitys, :dependent => :destroy
+
+  valdates :time_of_day, :uniqueness => { :scope => :day_of_week }
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -4,4 +4,5 @@ class Tutor < ActiveRecord::Base
   has_many :availabilitys, :dependent => :destroy
 
   validates :name, :uniqueness => true, :length => { :minimum => 2 }
+  validates :hours, :numericality => { :less_than_or_equal_to => 19.5, :greater_than_or_equal_to => 0 }
 end
