e930cfa5e8606de55634faf0c0b440ed1b35e0d8
hmrm <hmrmaxwell@gmail.com>
2012-07-23 14:10:16 -0700

    added json and xml to admin

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -2,7 +2,9 @@ class AvailabilitiesController < ApplicationController
 
   def admin
     @tutors = Tutor.all
+    @tutors_legible = Tutor.select('name, hours')
     @availabilities = {}
+    @availabilities_legible = Availability.joins(:shift_time).joins(:tutor).select('name, time_of_day, day_of_week, available')
     ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].each do |day|
       this_day = {}
       ShiftTime.where("day_of_week = '#{day}'").order("time_of_day DESC").each do |time|
@@ -19,7 +21,8 @@ class AvailabilitiesController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { render json: @availabilities }
+      format.json { render :json => {:availabilities => @availabilities_legible, :tutors => @tutors_legible} }
+      format.xml { render :xml => {:availabilities => @availabilities_legible, :tutors => @tutors_legible} }
     end
   end
 
